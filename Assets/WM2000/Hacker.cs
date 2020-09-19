e4f7d337072b89bab172f4c7598856e26d52799b
JP <jpjones22@gmail.com>
2020-09-19 10:43:49 -0700

    Initial Commit

---
+++ Assets/WM2000/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello Neo.  The Matrix has you.");
+        Terminal.WriteLine("Think about your way out.\nEach selection becomes more difficult.");
+        Terminal.WriteLine("However, the rewards increase as well.\nChoose the hack...");
+        Terminal.WriteLine("\n1) Library Server\n2) Bank Server\n3) CIA Operations.");
+        Terminal.WriteLine("Choose your destiny:");
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# This repo is for my educational purposes.
+
+## Author: 4a50 (JP Jones)
+
+All assets and code are developed by me, and is for use with an educational package.
+Any assets downloaded and used other than the core unity files and scripts are &copy; GameDev.tv
+
+Since you now know all that, feel free to take a look around and feedback is always welcome (including a little trolling).
+
+**4a50**
