48a424280c2a998641e3d3ed00607800f520779a
JP <jpjones22@gmail.com>
2020-09-20 17:03:39 -0700

    Start screen completed.  Program runs.

--- Assets/WM2000/Hacker.cs
+++ Assets/WM2000/Hacker.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    //Game State
+    public int level;
+    enum Screen { MainMenu, Password, MasterKey, Win};
+    Screen currentScreen;
+    public string userName = "Amy";
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +23,66 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu()
     {
-        string greeting = "Parker";        
+                
         Terminal.ClearScreen();        
-        Terminal.WriteLine("Hello " + greeting + ".  The Matrix has you.");
+        Terminal.WriteLine("Hello " + userName + ".  The Matrix has you.");
         Terminal.WriteLine("Think about your way out.\nEach selection becomes more difficult.");
         Terminal.WriteLine("However, the rewards increase as well.\nChoose the hack...");
         Terminal.WriteLine("\n1) Library Server\n2) Bank Server\n3) CIA Operations.");
         Terminal.WriteLine("Choose your destiny:");
+        currentScreen = Screen.MainMenu;
+
+        
+    }
+    
+    void OnUserInput(string userInput)
+    {
+        print(userInput);
+        if (userInput == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(userInput);
+        }
+    }
+
+    void RunMainMenu(string userInput)
+    {
+        if (userInput == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (userInput == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (userInput == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+       
+        else if (userInput == "4a50")
+        {
+            Terminal.WriteLine("Hello Mr. JP.  MASTERKEY=selected");
+            currentScreen = Screen.MasterKey;
+        }
+        else
+        {
+            Terminal.WriteLine("Invalid Choice.  Don't test me.");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have selected level " + level);
+        Terminal.WriteLine("Enter your passcode: ");
     }    
 }
+
+    
